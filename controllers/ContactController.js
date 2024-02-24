1f777bd4ddff6fc1995f850997a50cd5a67d297c
RajatAgrawal <rag7731@gmail.com>
2024-02-24 19:26:55 +0530

    contactPage

--- controllers/ContactController.js
+++ controllers/ContactController.js
@@ -1,4 +1,5 @@
 const ContactModel = require('../models/contact')
+const CourseModel = require('../models/course')
 const nodemailer = require('nodemailer');
 
 class ContactController {
@@ -15,7 +16,7 @@ class ContactController {
                 description:description,
                 user_id:id
             })
-            await result.save()
+            await result.save();
             this.sendEmail(name,email,phone,course,description)
             res.redirect('/home')
         }catch(err){
@@ -24,29 +25,29 @@ class ContactController {
     }
 
     static sendEmail = async (name,email,phone,course,description) => {
-        console.log(name,email,phone,course,description);
-        // let transporter = await nodemailer.createTransport({
-        //     //For Gmail
-        //     host: "smtp.gmail.com",
-        //     port: 587,
+        // console.log(name,email,phone,course,description);
+        let transporter = await nodemailer.createTransport({
+            //For Gmail
+            host: "smtp.gmail.com",
+            port: 587,
 
-        //     auth: {
-        //         user: "rag7731@gmail.com",
-        //         pass: "mkbztxzzuczdrkpq",//Two-step Verification Password
-        //     },
-        // });
-        // let info = await transporter.sendMail({
-        //     from: "test@gmail.com", // sender address
-        //     to: 'rag7731@gmail.com', // list of receivers
-        //     subject: ` Problem in ${course} form`, // Subject line
-        //     text: "Details :", // plain text body
-        //     // html body
-        //     html: `Name : <b>${name}</b> 
-        //     <br> Course : <b>${course}</b> 
-        //     <br> Phone No. : <b>${phone}</b> 
-        //     <br> Email : <b>${email}</b>
-        //     <br> Description : <b>${description}</b>` 
-        // });
+            auth: {
+                user: "rag7731@gmail.com",
+                pass: "mkbztxzzuczdrkpq",//Two-step Verification Password
+            },
+        });
+        let info = await transporter.sendMail({
+            from: "test@gmail.com", // sender address
+            to: 'rag7731@gmail.com', // list of receivers
+            subject: ` Problem in ${course} form`, // Subject line
+            text: "Details :", // plain text body
+            // html body
+            html: `Name : <b>${name}</b> 
+            <br> Course : <b>${course}</b> 
+            <br> Phone No. : <b>${phone}</b> 
+            <br> Email : <b>${email}</b>
+            <br> Description : <b>${description}</b>` 
+        });
     }
 }
 
